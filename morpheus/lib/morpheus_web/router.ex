d6ec27f5fb709126af44297b61b5410ce9088d70
Michał Nowak <mhnowakk@gmail.com>
2023-10-18 22:40:47 +0200

    fix: remove brackets

--- morpheus/lib/morpheus_web/router.ex
+++ morpheus/lib/morpheus_web/router.ex
@@ -2,12 +2,12 @@ defmodule MorpheusWeb.Router do
   use MorpheusWeb, :router
 
   pipeline :api do
-    plug(:accepts, ["json"])
+    plug :accepts, ["json"]
   end
 
   scope "/api", MorpheusWeb do
-    pipe_through(:api)
-    resources("/products", ProductController, except: [:new, :edit])
+    pipe_through :api
+    resources "/products", ProductController, except: [:new, :edit]
   end
 
   scope "/api/swagger" do
