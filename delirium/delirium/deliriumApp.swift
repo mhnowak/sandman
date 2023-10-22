f7fc52b4241531f876befd56f483766a526ec762
Michał Nowak <mhnowakk@gmail.com>
2023-10-22 12:18:14 +0200

    feat: init swift app

---
+++ delirium/delirium/deliriumApp.swift
@@ -0,0 +1,17 @@
+//
+//  deliriumApp.swift
+//  delirium
+//
+//  Created by Michał Nowak on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct deliriumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
